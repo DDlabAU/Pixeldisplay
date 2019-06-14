8d02cc7a8c76cae7b257d82ca3fd66299f61ce86
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2019-06-14 15:53:56 +0200

    Add function that initializes the display in correct size and dimension

--- Skeletkode/P5js/Skeletkode/Skeletkode.js
+++ Skeletkode/P5js/Skeletkode/Skeletkode.js
@@ -5,8 +5,18 @@ var WebSocketAddress = "ws://127.0.0.1:7890";
 function setup() {
 	createCanvas(600, 800);
 	socketSetup(WebSocketAddress);
+
+	initDisplay();
 }
 
 function draw() {
   background(0);
+}
+
+function initDisplay() {
+  // Map an 12x15 grid of LEDs to the center of the window, scaled to take up most of the space
+  opc.ledGrid(0 * 64, 12, 4, width * 1/2, height * 1/5, width/13, height/17, 0, false, false);
+  opc.ledGrid(1 * 64, 12, 4, width * 1/2, height * 2/5, width/13, height/17, 0, false, false);
+  opc.ledGrid(2 * 64, 12, 4, width * 1/2, height * 3/5, width/13, height/17, 0, false, false);
+  opc.ledGrid(3 * 64, 12, 4, width * 1/2, height * 4/5, width/13, height/17, 0, false, false);
 }
